645912aa6fabe8adc290a28c67a83cba3617df20
conrado-v <93201156+conrado-v@users.noreply.github.com>
2022-03-10 17:30:46 -0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,44 +1,55 @@
 import turtle as t
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.head = self.snake[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_piece = t.Turtle(shape='square')
-            new_piece.color('white')
-            new_piece.penup()
-            new_piece.goto(position)
-            self.snake.append(new_piece)
+            self.add_piece(position)
+
+    def add_piece(self, position):
+        new_piece = t.Turtle(shape='square')
+        new_piece.color('white')
+        new_piece.penup()
+        new_piece.goto(position)
+        self.snake.append(new_piece)
+
+    def extend(self):
+        self.add_piece(self.snake[-1].position())
 
     def move(self):
         for piece_num in range(len(self.snake) - 1, 0, -1):
             new_x = self.snake[piece_num - 1].xcor()
             new_y = self.snake[piece_num - 1].ycor()
             self.snake[piece_num].goto(new_x, new_y)
-        self.snake[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.snake[0].heading() != 270:
-            self.snake[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
             self.move()
 
     def down(self):
-        if self.snake[0].heading() != 90:
-            self.snake[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
             self.move()
 
     def left(self):
-        if self.snake[0].heading() != 0:
-            self.snake[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
             self.move()
 
     def right(self):
-        if self.snake[0].heading() != 180:
-            self.snake[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
             self.move()
